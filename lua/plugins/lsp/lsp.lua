1eaa47815f73475fda68bcf15c4cd8bdcc1f45ec
KaitoMuraoka <kaitoboshi@icloud.com>
2024-07-27 12:34:46 +0900

    create mason

--- lua/plugins/lsp/lsp.lua
+++ lua/plugins/lsp/lsp.lua
@@ -1,11 +1,33 @@
-return {
-	    "neovim/nvim-lspconfig",
-	    dependencies = {
-		    {'williamboman/mason.nvim', config = true},
-		    'williamboman/mason-lspconfig.nvim',
-	    },
+return {"neovim/nvim-lspconfig",
+event = { "BufReadPre", "BufNewFile" },
+dependencies = {
+	"hrsh7th/cmp-nvim-lsp",
+	{ "folke/neodev.nvim", opts = {} },
+},
 	config = function()
     local lspconfig = require('lspconfig')
+    local mason_lspconfig = require('mason-lspconfig')
+    vim.api.nvim_create_autocmd('LspAttach', {
+        desc = 'LSP Actions',
+        callback = function(args)
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {noremap = true, silent = true})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {noremap = true, silent = true})
+        end,
+    })
+
+local cmp_nvim_lsp = require("cmp_nvim_lsp")
+local capabilities = cmp_nvim_lsp.default_capabilities()
+
+
+    mason_lspconfig.setup_handlers({
+	    function (server_name)
+	    	lspconfig[server_name].setup({
+			capabilities = capabilities,
+		})
+	    end
+    })
+
+-- swift
 lspconfig.sourcekit.setup {
     capabilities = {
         workspace = {
@@ -16,8 +38,12 @@ lspconfig.sourcekit.setup {
     },
 }
 
-local cmp_nvim_lsp = require("cmp_nvim_lsp")
-local capabilities = cmp_nvim_lsp.default_capabilities()
+lspconfig.emmet_ls.setup {
+	capabilities = capabilities,
+	filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+}
+
+--lua
 lspconfig.lua_ls.setup{
 	capabilities = capabilities,
 	setting = {
@@ -31,14 +57,6 @@ lspconfig.lua_ls.setup{
 		},
 	},
 }
-
-    vim.api.nvim_create_autocmd('LspAttach', {
-        desc = 'LSP Actions',
-        callback = function(args)
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {noremap = true, silent = true})
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {noremap = true, silent = true})
-        end,
-    })
 end,
 
 }
---
+++ lua/plugins/lsp/mason.lua
@@ -0,0 +1,43 @@
+return {
+    "williamboman/mason.nvim",
+    dependencies = {
+	    "williamboman/mason-lspconfig.nvim",
+	    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    config = function ()
+	    local mason = require("mason") -- import mason
+	    local mason_lsp_config = require("mason-lspconfig") -- import mason-lspconfig
+	    local mason_tool_installer = require("mason-tool-installer")
+
+	    mason.setup {
+		    ui = {
+			    icons = {
+				    package_installed = "✓",
+				    package_pending = "➜",
+				    package_uninstalled = "✗",
+			    },
+		    },
+	    }
+
+	    mason_lsp_config.setup({
+		    -- list of servers for mason to install
+		    ensure_installed = {
+			    "html",
+			    "cssls",
+			    "lua_ls",
+			    "pyright",
+		    },
+	    })
+
+	    mason_tool_installer.setup({
+		    ensure_installed = {
+			    "prettier",
+			    "stylua",
+			    "isort",
+			    "black",
+			    "pylint",
+			    "eslint_d",
+		    },
+	    })
+    end,
+}
