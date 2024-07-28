dfb5ad982979d87493aaaec809dba4c5ebe6637e
KaitoMuraoka <kaitoboshi@icloud.com>
2024-07-28 09:17:03 +0900

    delete

--- lua/config/options.lua
+++
@@ -1,6 +0,0 @@
-local opt = vim.opt
-
-opt.number = true
-opt.wrap = true -- 行の折り返しを有効にする
-
-opt.relativenumber = false -- 絶対値行番号にする
--- lua/plugins/toggleterm.lua
+++
@@ -1,9 +0,0 @@
-return {
-  -- amongst your other pluins
-  -- amongst your other plugins
-  "akinsho/toggleterm.nvim",
-  version = "*",
-  config = function()
-    require("toggleterm").setup({})
-  end,
-}
