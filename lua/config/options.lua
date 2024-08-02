affc144ca895cf73ec30667453c6a716e280c0b1
KaitoMuraoka <kaitoboshi@icloud.com>
2024-08-02 13:16:29 +0900

    相対行番号をtrueに修正

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,6 +3,6 @@ local opt = vim.opt
 opt.number = true -- 行番号を有効
 opt.wrap = true -- 行の折り返しを有効にします
 
-opt.relativenumber = false -- 絶対値行番号にする
+opt.relativenumber = true -- 絶対値行番号にする
 
 opt.clipboard = "unnamed" -- ヤンクをクリップボードに送る
