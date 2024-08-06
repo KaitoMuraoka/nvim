951b7ba2c0ef6d9cfef32a1f3e1877451a446dad
KaitoMuraoka <kaitoboshi@icloud.com>
2024-08-06 18:42:46 +0900

    options を追加

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,15 +1,19 @@
 local opt = vim.opt
 
+-- ファイル
+opt.fileencoding = "utf-8" -- エンコーディングをutf-8に変更
+opt.helplang = "ja" -- ヘルプファイルの言語は日本語
+
 opt.number = true -- 行番号を有効
 opt.wrap = true -- 行の折り返しを有効にします
 
 opt.relativenumber = true -- 絶対値行番号にする
 
 -- tabs & indentation
-opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 2 -- 2 spaces for indent width
-opt.expandtab = true -- expand tab to spaces
-opt.autoindent = true -- copy indent from current line when starting new one
+opt.tabstop = 2 -- タブ幅を2に設定
+opt.shiftwidth = 2 -- シフト幅を2に設定
+opt.expandtab = true -- タブ文字をスペースに置き換える
+opt.autoindent = true -- 自動インデントを有効にする
 
 -- clipboard
 opt.clipboard = "unnamed" -- ヤンクをクリップボードに送る
