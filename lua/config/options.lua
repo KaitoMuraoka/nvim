bc4f0985b502290dd68e5d4ac786df60078bb61a
KaitoMuraoka <kaitoboshi@icloud.com>
2024-08-06 18:34:48 +0900

    fix indentation

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,4 +5,11 @@ opt.wrap = true -- 行の折り返しを有効にします
 
 opt.relativenumber = true -- 絶対値行番号にする
 
+-- tabs & indentation
+opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line when starting new one
+
+-- clipboard
 opt.clipboard = "unnamed" -- ヤンクをクリップボードに送る
