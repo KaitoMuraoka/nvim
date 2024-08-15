8a7e81becda24e97bf9247ccfd125527fbe8193e
KaitoMuraoka <kaitoboshi@icloud.com>
2024-08-15 14:19:07 +0900

    add color Kanagawa

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,5 +8,14 @@ return {
 	-- end,
 
 	-- Title: noctis-high-contrast
-	"iagorrr/noctishc.nvim",
+	-- "iagorrr/noctishc.nvim",
+
+	-- Title: Kanagawa
+	"rebelot/kanagawa.nvim",
+	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	priority = 1000, -- make sure to load this before all the other start plugins
+	config = function()
+		-- load the colorscheme here
+		vim.cmd([[colorscheme kanagawa]])
+	end,
 }
