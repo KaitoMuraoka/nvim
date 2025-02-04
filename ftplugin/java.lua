1fcec439e9fc7da687b21f8e8de45d6531056c50
KaitoMuraoka <kaitoboshi@icloud.com>
2025-02-04 16:59:35 +0900

    mason のパスに変更

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,5 +1,5 @@
 local config = {
-    cmd = {'/opt/homebrew/bin/jdtls'},
+    cmd = {'/Users/kaitomuraoka/.local/share/nvim/mason/bin/jdtls'},
     root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
 }
 require('jdtls').start_or_attach(config)
