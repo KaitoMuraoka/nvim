27066cfd246bfebe872c30135c612a960f148ebe
KaitoMuraoka <kaitoboshi@icloud.com>
2025-02-11 11:26:59 +0900

    fix cmp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,53 +1,95 @@
 return {
   {
     "hrsh7th/nvim-cmp",
-    version = false,
-    event = "InsertEnter",
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/cmp-nvim-lua",
+      "saadparwaiz1/cmp_luasnip",
+      "L3MON4D3/LuaSnip",
     },
     config = function()
       local cmp = require('cmp')
       local luasnip = require('luasnip')
+
+      -- Load friendly snippets
+      require("luasnip.loaders.from_vscode").lazy_load()
+
       local opts = {
-        sources = cmp.config.sources {
-          { name = "nvim_lsp", },
-          { name = "path", },
-          { name = "buffer", },
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
         },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "path" },
+          { name = "buffer" },
+          { name = "nvim_lua" },
+        }),
         mapping = cmp.mapping.preset.insert({
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<tab>"] = cmp.mapping(function(original)
-            print("tab pressed")
+          ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
-              print("cmp expand")
               cmp.select_next_item()
             elseif luasnip.expand_or_jumpable() then
-              print("snippet expand")
               luasnip.expand_or_jump()
             else
-              print("fallback")
-              original()
+              fallback()
             end
-          end, {"i", "s"}),
-          ["<S-tab>"] = cmp.mapping(function(original)
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_prev_item()
-            elseif luasnip.expand_or_jumpable() then
+            elseif luasnip.jumpable(-1) then
               luasnip.jump(-1)
             else
-              original()
+              fallback()
             end
-          end, {"i", "s"}),
-
-        })
+          end, { "i", "s" }),
+          ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+          ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+          ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+          ["<C-e>"] = cmp.mapping({
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close(),
+          }),
+        }),
       }
+
       cmp.setup(opts)
+
+      -- Setup buffer source for `/` and `?` in command-line mode.
+      cmp.setup.cmdline({ '/', '?' }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+
+      -- Setup cmdline & path source for ':' in command-line mode.
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+          { name = 'cmdline' }
+        })
+      })
     end,
   },
   { "hrsh7th/cmp-nvim-lsp", lazy = true },
   { "hrsh7th/cmp-path", lazy = true },
   { "hrsh7th/cmp-buffer", lazy = true },
+  { "hrsh7th/cmp-cmdline", lazy = true },
+  { "hrsh7th/cmp-nvim-lua", lazy = true },
+  { "saadparwaiz1/cmp_luasnip", lazy = true },
+  { "L3MON4D3/LuaSnip", lazy = true },
 }
