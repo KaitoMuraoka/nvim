57fa2cb59df91e89d4b3e92e5417c5cb072f73ad
KaitoMuraoka <kaitoboshi@icloud.com>
2026-02-07 00:09:08 +0900

    Toggle term を追加

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -181,6 +181,42 @@ return {
     end,
   },
 
+  -- ターミナル
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    keys = {
+      { "<C-\\>", desc = "Toggle terminal" },
+      { "<leader>tf", desc = "Float terminal" },
+      { "<leader>th", desc = "Horizontal terminal" },
+      { "<leader>tv", desc = "Vertical terminal" },
+    },
+    config = function()
+      require("toggleterm").setup({
+        size = function(term)
+          if term.direction == "horizontal" then
+            return 15
+          elseif term.direction == "vertical" then
+            return vim.o.columns * 0.4
+          end
+        end,
+        open_mapping = [[<C-\>]],
+        direction = "horizontal",
+        float_opts = {
+          border = "curved",
+        },
+      })
+
+      -- キーマップ
+      vim.keymap.set("n", "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", { desc = "Float terminal" })
+      vim.keymap.set("n", "<leader>th", "<cmd>ToggleTerm direction=horizontal<cr>", { desc = "Horizontal terminal" })
+      vim.keymap.set("n", "<leader>tv", "<cmd>ToggleTerm direction=vertical<cr>", { desc = "Vertical terminal" })
+
+      -- ターミナルモードでのエスケープ
+      vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
+    end,
+  },
+
   -- Org-mode
   {
     "nvim-orgmode/orgmode",
