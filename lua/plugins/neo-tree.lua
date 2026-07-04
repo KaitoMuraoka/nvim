d559d6a89297e64746e3495353a5a9532aa18482
KaitoMuraoka <70003919+KaitoMuraoka@users.noreply.github.com>
2026-07-04 11:27:55 +0900

    feature/feat neo tree (#10)
    - **neo-treeに相対行番号を追加**
    - **隠しファイルは常に表示するように修正**

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -20,15 +20,25 @@ return {
         -- netrw を無効化し、ディレクトリを開いた際は現在のウィンドウを
         -- neo-tree に置き換える（旧 nvim-tree の挙動に合わせる）
         hijack_netrw_behavior = "open_current",
+        filtered_items = {
+          -- ドットファイルなど隠しファイルを常に表示する
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
       },
     })
 
     -- neo-tree のツリーウィンドウでも行番号・相対行番号を表示する
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = "neo-tree",
+    -- neo-tree はウィンドウ生成時に `setlocal nonumber norelativenumber` を
+    -- 強制実行するため、FileType だけでなく WinEnter でも上書きする
+    vim.api.nvim_create_autocmd({ "FileType", "WinEnter", "BufWinEnter" }, {
       callback = function()
-        vim.opt_local.number = true
-        vim.opt_local.relativenumber = true
+        if vim.bo.filetype == "neo-tree" then
+          vim.schedule(function()
+            vim.opt_local.number = true
+            vim.opt_local.relativenumber = true
+          end)
+        end
       end,
     })
   end,
