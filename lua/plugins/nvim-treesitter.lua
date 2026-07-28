4a83178bfe2249e5cf87a793e1d27e2660a2aa33
KaitoMuraoka <70003919+KaitoMuraoka@users.noreply.github.com>
2026-07-28 12:12:30 +0900

    シェルスクリプトの開発セットアップ (#13)
    - bashls 個別設定を追加して、shellcheckと明示的に連携させる
    - conform.nvim のフォーマッタ対象に bash filetype を追加() shfmt

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,42 +1,40 @@
 return {
   -- Treesitter: シンタックスハイライト
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    main = "nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "lua",
-        "vim",
-        "vimdoc",
-        "javascript",
-        "typescript",
-        "tsx",
-        "python",
-        "html",
-        "css",
-        "json",
-        "yaml",
-        "markdown",
-        "markdown_inline",
-        "bash",
-        "go",
-        "rust",
-        "c",
-        "cpp",
-        "prisma",
-        "kotlin",
-        "java",
-        "swift",
-      },
-      sync_install = false,
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = {
-        enable = true,
-      },
-    },
+  "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  build = ":TSUpdate",
+  event = { "BufReadPost", "BufNewFile" },
+  config = function()
+    require("nvim-treesitter").install({
+      "lua",
+      "vim",
+      "vimdoc",
+      "javascript",
+      "typescript",
+      "tsx",
+      "python",
+      "html",
+      "css",
+      "json",
+      "yaml",
+      "markdown",
+      "markdown_inline",
+      "bash",
+      "go",
+      "rust",
+      "c",
+      "cpp",
+      "prisma",
+      "kotlin",
+      "java",
+      "swift",
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function()
+        pcall(vim.treesitter.start)
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+  end,
 }
