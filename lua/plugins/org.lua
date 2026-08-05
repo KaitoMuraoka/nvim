1260e7028a306fe731ad20cc34a09e3a2a99c50a
KaitoMuraoka <70003919+KaitoMuraoka@users.noreply.github.com>
2026-08-05 10:53:09 +0900

    ファイルを作り直し修正

--- lua/plugins/org.lua
+++ lua/plugins/org.lua
@@ -5,7 +5,23 @@ return {
 	config = function()
 		-- Setup orgmode
 		require("orgmode").setup({
-			org_default_notes_file = "~/orgfiles/notes.org",
+			org_agenda_files = {
+				"~/org/*.org",
+				"~/org/projects/*.org",
+			},
+			org_default_notes_file = "~/org/inbox.org",
+			org_capture_templates = {
+				t = {
+					description = "Task (inbox)",
+					template = "* TODO %?\n  %U",
+					target = "~/org/inbox.org",
+				},
+				r = {
+					description = "Routine",
+					template = "* TODO %?\n  SCHEDULED: %t",
+					target = "~/org/routine.org",
+				},
+			},
 		})
 
 		-- Experimental LSP support
