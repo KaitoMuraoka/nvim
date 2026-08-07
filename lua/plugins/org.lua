09f3b38c1eb0b21d6c7d902a9ade327904041884
KaitoMuraoka <70003919+KaitoMuraoka@users.noreply.github.com>
2026-08-07 15:13:30 +0900

    org-mode の設定を修正 (#20)
    - **ファイルを作り直し修正**
    - **tilを追加**

--- lua/plugins/org.lua
+++ lua/plugins/org.lua
@@ -5,7 +5,24 @@ return {
 	config = function()
 		-- Setup orgmode
 		require("orgmode").setup({
-			org_default_notes_file = "~/orgfiles/notes.org",
+			org_agenda_files = {
+				"~/org/*.org",
+				"~/org/projects/*.org",
+			},
+			org_default_notes_file = "~/org/inbox.org",
+			org_capture_templates = {
+				t = {
+					description = "Task (inbox)",
+					template = "* TODO %?\n  %U",
+					target = "~/org/inbox.org",
+				},
+				l = {
+					description = "Today I Learned",
+					template = "* %?",
+					target = "~/org/til.org",
+					datetree = true,
+				},
+			},
 		})
 
 		-- Experimental LSP support
